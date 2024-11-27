04734bf9f358705aec54638ec1ca6143031b8e44
ramanshmk <vivabarca199684@gmail.com>
2024-11-27 16:59:10 +0300

    develop order usdc/sol

--- src/main/java/org/store/bot/WalletService.java
+++ src/main/java/org/store/bot/WalletService.java
@@ -1,20 +1,24 @@
 package org.store.bot;
 
+import com.mmorrell.openbook.manager.OpenBookManager;
+import com.mmorrell.openbook.model.OpenBookMarket;
+import com.mmorrell.serum.model.*;
+import com.mmorrell.serum.program.SerumProgram;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.p2p.solanaj.core.Account;
 import org.p2p.solanaj.core.PublicKey;
 import org.p2p.solanaj.core.Transaction;
+import org.p2p.solanaj.core.TransactionInstruction;
+import org.p2p.solanaj.programs.AssociatedTokenProgram;
+import org.p2p.solanaj.programs.Program;
 import org.p2p.solanaj.programs.SystemProgram;
 import org.p2p.solanaj.programs.TokenProgram;
 import org.p2p.solanaj.rpc.RpcException;
+import org.p2p.solanaj.rpc.types.ConfirmedTransaction;
 import org.p2p.solanaj.rpc.types.TokenResultObjects;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 @Service
 @RequiredArgsConstructor
@@ -27,14 +31,25 @@ public class WalletService {
     // Перевод токена между двумя токен-счётами
     public void buyToken() {
         // Ваш приватный ключ (отправитель)
-        Account senderAccount = new Account(getSecretKeyWalletFromFile());
-
+        Account senderAccount = new Account(getSecretKeyWallet());
         // Адрес токена и его MINT
         String tokenMintAddress = "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU";
 
-        // 4. Получение данных о токен счетах двух кошельков, по mint адресу токена
+        // Получение данных о токен-счетах двух кошельков, по mint адресу токена
         PublicKey sellerTokenAccount;
         PublicKey buyerTokenAccount;
+        Transaction transaction_0 = new Transaction();
+
+        // Динамическое создание токен-счёта для кошелька
+        TransactionInstruction transactionInstruction = AssociatedTokenProgram.create(senderAccount.getPublicKey(), senderAccount.getPublicKey(), PublicKey.valueOf(tokenMintAddress));
+        transaction_0.addInstruction(transactionInstruction);
+        try {
+            solanaClient.getRpcClient().getApi().sendTransaction(transaction_0, senderAccount);
+        } catch (RpcException e) {
+            log.warn("Token account for this wallet already exists: {}", e.getMessage());
+        }
+
+        // Получение токен-счётов у кошельков
         try {
             buyerTokenAccount = solanaClient.getRpcClient().getApi().getTokenAccountsByOwner(new PublicKey("CtBYeeLc9rCY3X4TwXvTwU79zgNrk3fmgDfpr99SxiKV"), new PublicKey(tokenMintAddress));
             sellerTokenAccount = solanaClient.getRpcClient().getApi().getTokenAccountsByOwner(new PublicKey("GrNg1XM2ctzeE2mXxXCfhcTUbejM8Z4z4wNVTy2FjMEz"), new PublicKey(tokenMintAddress));
@@ -43,24 +58,55 @@ public class WalletService {
             throw new RuntimeException(e);
         }
 
-        // 5. Подготовьте транзакцию для перевода токенов
-        Transaction transaction = new Transaction();
-        transaction.addInstruction(
-                TokenProgram.transfer(
-                        sellerTokenAccount, // От кого (токен-счёт)
-                        buyerTokenAccount, // Кому (токен-счёт)
-                        20000,                            // Количество токенов в лампортах
-                        new PublicKey("GrNg1XM2ctzeE2mXxXCfhcTUbejM8Z4z4wNVTy2FjMEz")   // Отправитель (ваш кошелёк)
-                )
-        );
+        final Market solUsdcMarket = new MarketBuilder()
+                .setClient(solanaClient.getRpcClient())
+                .setPublicKey(new PublicKey("C3YPL3kYCSYKsmHcHrPWx1632GUXGqi2yMXJbfeCc57q"))
+                .setRetrieveOrderBooks(true)
+                .build();
 
-        // 7. Отправьте транзакцию в сеть
+        OpenBookManager openBookManager = new OpenBookManager(solanaClient.getRpcClient());
+
+//        OpenBookMarket solUsdcMarket = openBookManager.getMarket(
+//                PublicKey.valueOf("C3YPL3kYCSYKsmHcHrPWx1632GUXGqi2yMXJbfeCc57q"),
+//                false, // Не загружать рынок заново, если он уже кэширован
+//                true   // Загружать ордербуки
+//        ).get();
+//
+//        log.info("Bids: {}", solUsdcMarket.getBidOrders()); // Заявки на покупку
+//        log.info("Asks: {}", solUsdcMarket.getAskOrders()); // Заявки на продажу
+
+        // 3. Создайте лимитный ордер
+        Order order = Order.builder()
+                .price(1000000000)  // цена, симулируем очень высокую цену, чтобы ордер был выполнен сразу, а не ждал указанную желаемую цену за токен
+                .quantity(25)  // Количество
+                .owner(new PublicKey("CtBYeeLc9rCY3X4TwXvTwU79zgNrk3fmgDfpr99SxiKV"))
+                .orderTypeLayout(OrderTypeLayout.LIMIT)  // Укажите правильный тип
+                .selfTradeBehaviorLayout(SelfTradeBehaviorLayout.DECREMENT_TAKE)  // Укажите правильное поведение
+                .buy(true)
+                .build();
+
+
+
+        OpenOrdersAccount openOrdersAccount = SerumUtils.findOpenOrdersAccountForOwner(solanaClient.getRpcClient(), solUsdcMarket.getOwnAddress(), senderAccount.getPublicKey());
+
+        // Отправка ордера на рынок
+        TransactionInstruction placeOrderInstruction = SerumProgram.placeOrder(
+                senderAccount, buyerTokenAccount, openOrdersAccount.getOwnPubkey(), new Market(), order, null);
+
+        // Подготовьте транзакцию для перевода токенов
+        Transaction transaction = new Transaction();
+        transaction.addInstruction(placeOrderInstruction);
+
+        // Отправьте транзакцию в сеть
         try {
             String signature = solanaClient.getRpcClient().getApi().sendTransaction(transaction, senderAccount);
             log.info("Transaction Signature: " + signature);
+            log.info("Transaction status: {}", solanaClient.getRpcClient().getApi().getTransaction(signature));
         } catch (RpcException e) {
             throw new RuntimeException(e);
         }
+
+
     }
 
     public Double getTokenBalance(String token) {
@@ -89,7 +135,7 @@ public class WalletService {
         PublicKey toPublickKey = new PublicKey(toWalletAddress);
         int lamports = amount;
 
-        Account signer = new Account(getSecretKeyWalletFromFile());
+        Account signer = new Account(getSecretKeyWallet());
 
         Transaction transaction = new Transaction();
         transaction.addInstruction(SystemProgram.transfer(fromPublicKey, toPublickKey, lamports));
@@ -105,16 +151,9 @@ public class WalletService {
         return String.format("Successfully transferred %s to %s, result: %s", fromWalletAddress, toWalletAddress, signature);
     }
 
-    // Получение приватного ключа из локального файла
-    private byte[] getSecretKeyWalletFromFile() {
-        String content;
-        try {
-            content = Files.readString(Path.of(fileKey));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        String[] intStrings = content.replaceAll("[^0-9,\\-]", "").split(",");
+    // Получение приватного ключа
+    private byte[] getSecretKeyWallet() {
+        String[] intStrings = fileKey.replaceAll("[^0-9,\\-]", "").split(",");
 
         byte[] secretKey = new byte[intStrings.length];
 
